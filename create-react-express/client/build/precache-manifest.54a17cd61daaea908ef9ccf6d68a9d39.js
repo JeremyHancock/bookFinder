a45b7ab48326c5b4f31d1554619ba30729bbe442
doulos tou Christou <jeremyhancock32@gmail.com>
2019-01-11 16:51:23 -0500

    build done

---
+++ create-react-express/client/build/precache-manifest.54a17cd61daaea908ef9ccf6d68a9d39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4ab10a18be6073541181",
+    "url": "/static/js/main.4ab10a18.chunk.js"
+  },
+  {
+    "revision": "cbf27b399147ce740494",
+    "url": "/static/js/1.cbf27b39.chunk.js"
+  },
+  {
+    "revision": "4ab10a18be6073541181",
+    "url": "/static/css/main.d6a36044.chunk.css"
+  },
+  {
+    "revision": "1cc2f8716d747c5aa782d59513970a68",
+    "url": "/index.html"
+  }
+];
